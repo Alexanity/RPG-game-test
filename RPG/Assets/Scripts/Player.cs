1c346c705cbb4c2cfabfbbc722b69d2ef4663e82
Alexanity <80948589+Alexanity@users.noreply.github.com>
2023-02-22 17:27:40 +0200

    Well this is fun

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -4,54 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta; // difference between position rn and where the player will be -> between frames
-    private RaycastHit2D hit;
-    private void Start()
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+
+    void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        body = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate() // follows the same frame as the physics
+    void Update()
     {
-        
-       // Physics2D.queriesStartInColliders = false;
-        //Reset MoveDelta
-        moveDelta = Vector3.zero;
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
 
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");   
-        
-        moveDelta = new Vector3(x,y,0);
-
-        // testing
-        //Debug.Log(x);
-        //Debug.Log(y);
-
-        //Swap sprite direction, depending on wether you're going right or left
-        if(moveDelta.x > 0)
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
         {
-            transform.localScale = Vector3.one;
-        }
-        else if(moveDelta.x < 0)
-        {
-            transform.localScale = new Vector3(-1,1,1);
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
         }
 
-        // Making sure we can move in this direction by casting a box there first/ if the box is null we're free to move
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if(hit.collider != null)
-        {
-
-            //Movement
-            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
-        }
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider != null)
-        {
-
-            //Movement
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
